03195eb0025e0023201c1ee55322a735045bbe84
Michael Koegel <mkoegel@gmail.com>
2016-02-27 09:03:50 -0700

    Created simple grade book
    this grade book calculates different averages for a list of grades.

--- Test/com/PracticeWork/compute_grades.py
+++ Test/com/PracticeWork/compute_grades.py
@@ -2,14 +2,45 @@
 Created on Feb 26, 2016
 
 @author: michael.f.koegel
+
+This is going to turn into a simple grade book that performs
+certain calculations based on a set list (or a list that gets
+passed to the  grades_sum function).
 '''
+#defining the hard coded list
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
-def grades_sum(scores):
+def print_grades(grades):
+    for grade in grades:
+        print(grade)
+
+def grades_sum(grades):
     total = 0
     
-    for score in scores:
-        total += score
-    print(total)
+    for grade in grades: 
+        total += grade
+    return total
+    
+def grades_average(grades):
+    sum_of_grades = grades_sum(grades)
+    average = sum_of_grades / float(len(grades))
+    return average
 
-grades_sum(grades)
+def grades_variance(scores):
+    average = grades_average(scores)
+    variance = 0
+    
+    for score in scores:
+        variance += ((average - score) ** 2)
+    
+    return average / len(scores)
+
+def grades_std_deviation(variance):
+    return (variance ** 0.5)
+    
+variance = grades_variance(grades)
+#print("The list of grades: " + str(print_grades(grades)))
+print("The sum of the grades is " + str(grades_sum(grades)))
+print("The average of the grades is " + str(grades_average(grades)))
+print("The grade variance is " + str(grades_variance(grades)))
+print("The grade standard deviation is " + str(grades_std_deviation(variance)))
