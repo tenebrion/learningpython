3a81853e30626d05ee4f6fc291f2a67e9f0dc6aa
Michael Koegel <mkoegel@gmail.com>
2016-03-01 10:02:49 -0700

    simple change
    small changes to mytest

--- Test/com/PracticeWork/mytest.py
+++ Test/com/PracticeWork/mytest.py
@@ -1,3 +1,4 @@
+'''
 def multiply(numbers):
 	total = 1
 	
@@ -6,3 +7,9 @@ def multiply(numbers):
 	print(total)
 
 multiply([1, 3, 5, 3, 2])
+'''
+
+
+garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
+message = garbled[::-2]
+print(message)
