f6fee35eeaf268e9df49c4f6710706752409470a
Michael Koegel <mkoegel@gmail.com>
2016-03-18 10:04:37 -0700

    Working through Project Euler problems.

--- pe03.py
+++ pe03.py
@@ -3,10 +3,17 @@ Largest Prime Factor
 
 What is the largest prime factor of the number 600851475143?
 """
-large_prime = 600851475143
-i = 2
-while i * i < large_prime:
-    while large_prime % i == 0:
-        large_prime = large_prime / i
-    i = i + 1
-print(large_prime)
+def find_prime():
+    """
+    This is problem 3 on Project Euler
+    :return:
+    """
+    large_prime = 600851475143
+    i = 2
+    while i * i < large_prime:
+        while large_prime % i == 0:
+            large_prime = large_prime / i
+        i += 1
+    return large_prime
+
+print(find_prime())
---
+++ pe04.py
@@ -0,0 +1,22 @@
+"""
+Largest palindrome product
+
+Problem 4
+A palindromic number reads the same both ways.
+The largest palindrome made from the product
+of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the
+product of two 3-digit numbers.
+"""
+product = 0
+
+for x in range(999, 100, -1):
+    for y in range(x, 100, -1):
+        z = x * y
+        if z > product:
+            s = str(x * y)
+            if s == s[::-1]:
+                 product = x * y
+
+print(product)
