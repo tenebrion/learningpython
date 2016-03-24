9da0ecd8edb60b5ea8dc6c3d2c023dc432fdb579
Michael Koegel <mkoegel@gmail.com>
2016-03-24 08:06:36 -0700

    Fixing comments... PEP8 standards for comments and variables

--- CheckProfanity.py
+++ CheckProfanity.py
@@ -1,31 +1,38 @@
-'''
+"""
 Created on Feb 25, 2016
 
 @author: michael.f.koegel
-'''
-#need to import the urllib file to open URLs
+"""
 import urllib.request
 
-#defining my function that will read text from a .txt file
-def readText():
-    quotes = open(r"C:\Users\michael.f.koegel\Documents\Python\MovieQuotes.txt") #actually opening the file
-    contentsOfFile = quotes.read() #this will read the file
-    #print(contentsOfFile) #debugging via print method
-    quotes.close() #closing the file so it doesn't remain open in memory
-    checkProfanity(contentsOfFile) #passing the file to the checkProfanity function
 
-#this function actually connect to the web site to check for profanity in our text document
-def checkProfanity(textToCheck):
-    connection = urllib.request.urlopen("http://www.wdyl.com/profanity?q="+textToCheck) #opening the url
-    output = connection.read() #reading the site
-    #print(output) #debugging mode via print
-    connection.close() #closing the connection
+def read_text():
+    """
+    This method simple opens the file and reads it, then passes the info
+    off to check for profanity.
+    """
+    quotes = open(r"C:\Users\michael.f.koegel\Documents\Python\MovieQuotes.txt")
+    contentsOfFile = quotes.read()
+    quotes.close()
+    check_profanity(contentsOfFile)
+
+
+def check_profanity(text_check):
+    """
+    This method opens a connection to a site and checks to see if our
+    content has profanity. If it does, a message stating so is produced.
+
+    :param text_check: str
+    """
+    connection = urllib.request.urlopen("http://www.wdyl.com/profanity?q=" + text_check)
+    output = connection.read()
+    connection.close()
     
-    if "true" in output: #if the site says there is profanity, it will print a message
+    if "true" in output:
         print("Profanity Alert!!")
-    elif "false" in output: #if the site says there is no profanity, prints a message
+    elif "false" in output:
         print("This document has no curse words!")
-    else: #otherwise the file cannot be read
+    else:
         print("Could not scan the document properly!")
     
-readText() #initializing the method
+read_text()
