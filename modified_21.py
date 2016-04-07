7eb7ed76cd03b18e4a2798052c80e22e0ad3f9d3
Michael Koegel <mkoegel@gmail.com>
2016-04-07 13:28:15 -0700

    Starting from scratch

--- modified_21.py
+++ modified_21.py
@@ -45,149 +45,23 @@ round, make the deck have all of the cards again.
 import random
 
 
-class Card:
+def make_deck(num_decks):
     """
-    Creating the basis for a playing card
-    """
-    def __init__(self, value=1, rank="Ace", suit="Spades"):
+        Creating the standard 52-card deck
+        I'd also like to see about creating the card value in here.
         """
-        :param value: value of  blackjack card
-        :param rank: type of card (e.g. King)
-        :param suit: Diamonds, Hearts, Spades, Clubs
-        """
-        self.value = value
-        self.rank = rank
-        self.suit = suit
-
-    def __str__(self):
-        # just returning the rank and suit (e.g. 2 of Spades)
-        return "{} of {}".format(self.rank, self.suit)
-
-
-class Deck:
-    """
-    Creating the standard 52-card deck
-    I'd also like to see about creating the card value in here.
-    """
     ranks = {"Ace": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
              "8": 8, "9": 9, "10": 10, "Jack": 10, "Queen": 10, "King": 10}
     suits = ["Spades", "Diamonds", "Hearts", "Clubs"]
+    cards = []
 
-    def __init__(self, num_decks=1):
-        """
-        :param num_decks: this will control how many decks the player wants to use
-                          For example, if there are 2 decks, that's 104 cards in play
-        """
-        # storing the info passed back by class Card
-        self.cards = []
-        # storing all the cards in the deck
-        self.deck_cards = []
+    for i in range(num_decks):
+        for rank, value in ranks.items():
+            for suit in suits:
+                cards.append("{} of {}".format(rank, suit))
 
-        for rank, value in self.ranks.items():
-            for suit in self.suits:
-                playing_card = Card()
-                playing_card.rank = rank
-                playing_card.value = value
-                playing_card.suit = suit
-                self.cards.append(playing_card)
+    random.shuffle(cards)
+    return cards
 
-        # creating the playing card deck and shuffling the cards
-        for i in range(num_decks):
-            for card in self.cards:
-                self.deck_cards.append(card)
-        random.shuffle(self.deck_cards)
-
-    def draw_card(self):
-        """
-        drawing the last card in the deck and removing it from the 'stack'
-        :return:
-        """
-        return self.deck_cards.pop()
-
-    # thought this was an interesting way to keep tabs on the number of
-    # cards in the deck. I also found it online (Stackoverflow I think)
-    def __len__(self):
-        return len(self.deck_cards)
-
-
-class Score:
-    """
-    This class will keep track of the player and dealer scores. When the
-    game is over, is will pass along a ranking (e.g. 92 points = A)
-    """
-    def __init__(self):
-        self.scores = []
-
-    def ranking(self, score):
-        """
-        Simple rating system for the end score
-        :return:
-        """
-        if score >= 90:
-            return "A"
-        elif score >= 80:
-            return "B"
-        elif score >= 70:
-            return "C"
-        elif score >= 60:
-            return "D"
-        else:
-            return "F"
-
-
-class Hand:
-    """
-    This is where we'll store the player and dealer hands
-    """
-    def __init__(self):
-        # this will initialize anything I'm using
-        self.player_hand = []
-        self.dealer_hand = []
-
-        while len(self.player_hand) < 2:
-            self.player_hand.append(Deck().draw_card())
-
-        while len(self.dealer_hand) <= 2:
-            self.dealer_hand.append(Deck().draw_card())
-
-    def add_to_hand(self):
-        """
-        This will add cards to the player/dealer hand
-        :return:
-        """
-        for items in self.player_hand:
-            print("\n{}".format(items))
-            #return "\n{}".format(items)
-
-    def move(self):
-        """
-        This will enable things like hit, stay, bust, etc.
-        :return:
-        """
-        pass
-
-
-class Game:
-    """
-    Right now while I figure things out, these are here to help
-    test the classes I'm making.
-    """
-    deck = Deck()
-    for a_card in deck.deck_cards:
-        print(a_card)
-
-    Hand().add_to_hand()
-    # when cards are added to the list, they aren't getting removed yet
-    print("\nThere are {} cards left".format(deck.__len__()))
-
-    for turn in range(5):
-        print("Turn: ", turn + 1)
-
-        if turn == 4:
-            player_score = 82
-            letter_grade = Score().ranking(player_score)
-            print("Game Over. \n"
-                  "Your final score is {}, "
-                  "which results in the letter grade: {}".format(player_score, letter_grade))
-
-Game()
+for card in make_deck(2):
+    print(card)
