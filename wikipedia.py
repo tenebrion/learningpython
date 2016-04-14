a48ed862c5a608104c56abe404d458bd51ddd0de
Michael Koegel <mkoegel@gmail.com>
2016-04-14 16:29:10 -0700

    Trying to make some classes out of this.

--- wikipedia.py
+++ wikipedia.py
@@ -3,8 +3,33 @@ Creating a program to connect to Wikipedia and display content.
 """
 import json
 from urllib.request import urlopen
+import webbrowser
 
-url = "https://en.wikipedia.org//w/api.php?action=query&format=json&list=random"
-response = urlopen(url).read().decode('utf8')
-obj = json.loads(response)
-print(obj)
+
+
+
+class Wikipedia:
+    """
+    Testing this out
+    """
+    page = "https://en.wikipedia.org/wiki?curid="
+    url = "https://en.wikipedia.org//w/api.php?action=query&format=json&list=random"
+
+    def __init__(self, page):
+        self.page = page
+
+    def random_page(self):
+        """
+        simple method to randomly fetch new pages
+        :return:
+        """
+        response = urlopen(self.url).read().decode('utf8')
+        obj = json.loads(response)
+        description = obj["query"]["random"][0]["title"]
+        pageid = obj["query"]["random"][0]["id"]
+
+print("Welcome to my random Wikipedia page generator.\n"
+      "A page title will appear. If you would like to read the page, press y for yes.\n"
+      "Otherwise, select n for no, which will pull a different page.\n"
+      "When you are done, press enter to quit.")
+
