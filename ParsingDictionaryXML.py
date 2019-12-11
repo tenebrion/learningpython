e2d66a9dae0b6e6b344b4f43b5cbb3b48ae1daa4
Michael Koegel <mkoegel@gmail.com>
2019-12-10 19:10:59 -0500

    Update ParsingDictionaryXML.py

--- ParsingDictionaryXML.py
+++ ParsingDictionaryXML.py
@@ -3,7 +3,7 @@ import urllib.request
 
 #http://www.diveintopython.net/xml_processing/searching.html
 def main():
-    xml = urllib.request.urlopen("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/thug?key=2fb6218d-a259-4bb1-af32-db6fe195cdc5")
+    xml = urllib.request.urlopen("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/thug?key=")
     xml_file = parse(xml)
     ref_list = xml_file.getElementsByTagName("dt")
     print(ref_list[0].toxml())
